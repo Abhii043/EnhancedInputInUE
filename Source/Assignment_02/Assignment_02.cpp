0cb6bbf98c457dfb5cfa0528a6acce20c269ab24
Abhi <abhi.np.71@gmail.com>
2024-04-27 13:16:24 +0530

    Learning EnhancedInput in Unreal Engine

---
+++ Source/Assignment_02/Assignment_02.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Assignment_02.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Assignment_02, "Assignment_02" );
---
+++ Source/Assignment_02/Assignment_02GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Assignment_02GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASSIGNMENT_02_API AAssignment_02GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
